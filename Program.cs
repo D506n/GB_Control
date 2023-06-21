11ffb83df0440759d11ddc30b6bed26b99d5b266
Dmitriy Medvedev <D506n@yandex.ru>
2023-06-21 16:51:38 +0300

    Добавил метод EnterTextArray

--- Program.cs
+++ Program.cs
@@ -4,3 +4,19 @@
     string TempText = Console.ReadLine();
     return TempText;
 }
+string[] EnterTextArray(string MessageForUser)//меняю размер массива и записываю в него строки до тех пор, пока пользователь вводит строки с клавиатуры, таким образом программа не ограничена конкретными рамками в размерах массива из которого будут выбрираться короткие строки
+{
+    string[] array = Array.Empty<string>();
+    int EnterStep = 0;
+    for (string TempText = " "; TempText != "";)
+    {
+        TempText = EnterText(MessageForUser);
+        if (TempText != "")
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = TempText;
+            EnterStep++;
+        }
+    }
+    return array;
+}
